80680548f01822dbf6fee176cadaed0552c00637
David Hatch <dhatch387@gmail.com>
2016-04-19 16:08:33 -0400

    Hashtable: Add additional documentation.

--- hashtable/hashtable.h
+++ hashtable/hashtable.h
@@ -10,20 +10,51 @@
 
 #define DEFAULT_BUCKET_SIZE 5
 
-
+/**
+ * A hash bucket for Hashtable.  Stores at most DEFAULT_BUCKET_SIZE hash table
+ * values, using a hashing with chaining strategy.
+ */
 struct Bucket {
   LockRequestLinkedList* slots[DEFAULT_BUCKET_SIZE];
   int keys[DEFAULT_BUCKET_SIZE];
 };
 
-
+/**
+ * A hash table for storing lock requests.  The hash table supports
+ * granular locking.
+ *
+ * This lock table is backed by a preallocated region of memory, the size of
+ * which is determined by the initial size and the number of items in each
+ * bucket.
+ */
 class Hashtable {
 public:
+  /**
+   * Initialize a hashtable, with @n buckets preallocated
+   *
+   * The hashtable will be able to store at most @n*<DEFAULT_BUCKET_SIZE>
+   * keys.  If a bucket overflows, the result is undefined.
+   */
   Hashtable(int n);
+
+  /**
+   * Insert a lock request @lr into the table for @key.
+   */
   void lock_insert(int key, LockRequest& lr);
-  TNode<LockRequest>* get_list(int key);
+
+  /**
+   * Get a reference to the lock request list for @key.
+   */
   LockRequestLinkedList * latch_free_get_list(int key);
+
+  /**
+   * Remove a lock request @lr from @key
+   */
   void lock_delete(int key, TNode<LockRequest>* lr);
+
+  /**
+   * Get the mutex protecting a particular key
+   */
   pthread_mutex_t* get_mutex(int key);
 private:
   inline int hash(int key);
@@ -40,7 +71,7 @@ private:
   TNode<LockRequest>* memory_array;
   TNode<LockRequest>* memory_ptr;
 
-
+  // Bucket locks.
   pthread_mutex_t* lock_array;
   pthread_mutex_t global_lock;
 };
