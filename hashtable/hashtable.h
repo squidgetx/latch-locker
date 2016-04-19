2f570ca020a76fd83dcabd5e4fa7ceb35e337877
David Hatch <dhatch387@gmail.com>
2016-04-19 15:52:30 -0400

    Hashtable: Retab to spaces.

--- hashtable/hashtable.h
+++ hashtable/hashtable.h
@@ -2,9 +2,9 @@
 #define HASHTABLE_HASHTABLE_H
 
 #include <pthread.h>
-#include "LockRequestLinkedList.h"
 #include <new>
-#include <iostream>
+
+#include "LockRequestLinkedList.h"
 
 
 
@@ -12,31 +12,28 @@
 
 
 struct Bucket {
-	LockRequestLinkedList* slots[DEFAULT_BUCKET_SIZE];
-	int keys[DEFAULT_BUCKET_SIZE];
+  LockRequestLinkedList* slots[DEFAULT_BUCKET_SIZE];
+  int keys[DEFAULT_BUCKET_SIZE];
 };
 
 
 class Hashtable {
-	public:
+public:
+  Hashtable(int n);
+  void lock_insert(int key, LockRequest& lr);
+  TNode<LockRequest>* get_list(int key);
+  void lock_delete(int key, TNode<LockRequest>* lr);
+  pthread_mutex_t* get_mutex(int key);
+private:
+  int num_buckets;
+  LockRequestLinkedList* list_array;
+  TNode<LockRequest>* memory_array;
+  TNode<LockRequest>* memory_ptr;
 
-	 	Hashtable(int n);
-		void lock_insert(int key, LockRequest& lr);
-		TNode<LockRequest>* get_list(int key);
-		void lock_delete(int key, TNode<LockRequest>* lr);
-	 	pthread_mutex_t* get_mutex(int key);
 
- 		
-	private:
-	 	int num_buckets;
-	 	LockRequestLinkedList* list_array;
-		TNode<LockRequest>* memory_array;
-		TNode<LockRequest>* memory_ptr;
-
-		
-	 	pthread_mutex_t* lock_array;
-		pthread_mutex_t global_lock;
-		Bucket* bucket_array;
+  pthread_mutex_t* lock_array;
+  pthread_mutex_t global_lock;
+  Bucket* bucket_array;
 };
 
 #endif
