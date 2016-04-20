4b81fcecea01dc2eefcf5807006bfc4014eb6b75
David Hatch <dhatch387@gmail.com>
2016-04-20 16:03:14 -0400

    Reformat linked list.

--- hashtable/TLinkedList.h
+++ hashtable/TLinkedList.h
@@ -8,70 +8,69 @@ class TLinkedList
 {
 
 public:
-    TNode<T>* head;
-    TNode<T>* tail;
-    int isize;
+  TNode<T>* head;
+  TNode<T>* tail;
+  int isize;
 
-    TLinkedList() 
-    {
-        head = NULL;
-        tail = NULL;
-        isize = 0;
-    }
+  TLinkedList() 
+  {
+    head = NULL;
+    tail = NULL;
+    isize = 0;
+  }
 
 
-    void append(TNode<T> *n);
-    void prepend(TNode<T> *n);
+  void append(TNode<T> *n);
+  void prepend(TNode<T> *n);
 
-    void remove(TNode<T> *n);
-	int size();
+  void remove(TNode<T> *n);
+  int size();
 
 };
 
 template <class T>
 void TLinkedList<T>::append(TNode<T> *n) {
-    if (head == NULL) {
-        head = n;
-        tail = n;
-		isize = 1;
-    } else {
-        tail->next = n;
-        n->prev = tail;
-        tail = n;
-		isize++;
-    }
+  if (head == NULL) {
+    head = n;
+    tail = n;
+    isize = 1;
+  } else {
+    tail->next = n;
+    n->prev = tail;
+    tail = n;
+    isize++;
+  }
 }
 template <class T>
 void TLinkedList<T>::prepend(TNode<T> *n) {
-    if (head == NULL) {
-        head = n;
-        tail = n;
-		isize = 1;
-    } else {
-        head->prev = n;
-        n->next = head;
-        head = n;
-		isize++;
-    }
+  if (head == NULL) {
+    head = n;
+    tail = n;
+    isize = 1;
+  } else {
+    head->prev = n;
+    n->next = head;
+    head = n;
+    isize++;
+  }
 }
 
 template <class T>
 void TLinkedList<T>::remove(TNode<T> *n) {
-
-    if (n == head)
-        head = n->next;
-    if (n == tail)
-        tail = n->prev;
-    if (n->next != NULL)
-        n->next->prev = n->prev;
-    if (n->prev != NULL)
-        n->prev->next = n->next;
-	isize--;
+  if (n == head)
+    head = n->next;
+  if (n == tail)
+    tail = n->prev;
+  if (n->next != NULL)
+    n->next->prev = n->prev;
+  if (n->prev != NULL)
+    n->prev->next = n->next;
+  isize--;
 }
 
 template <class T>
 int TLinkedList<T>::size() {
-	return isize;
+  return isize;
 }
 
 #endif
