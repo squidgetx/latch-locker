40bf13f39fee8094e2593e87a22a72ce81fa5923
David Hatch <dhatch387@gmail.com>
2016-04-20 16:15:17 -0400

    Lock pool: Reformat.

--- hashtable/lock_pool.h
+++ hashtable/lock_pool.h
@@ -7,17 +7,15 @@
 
 class LockPool
 {
-  public:
-    LockPool(int n);
-    TNode<LockRequest>* get_uninit_lock();
-    MemoryChunk<TNode<LockRequest> > * get_uninit_locks(int n);
+public:
+  LockPool(int n);
+  TNode<LockRequest>* get_uninit_lock();
+  MemoryChunk<TNode<LockRequest> > * get_uninit_locks(int n);
 
-  private:
-
-    TNode<LockRequest>* memory_array;
-    TNode<LockRequest>* memory_ptr;
-    pthread_mutex_t pool_mutex;
-    
+private:
+  TNode<LockRequest>* memory_array;
+  TNode<LockRequest>* memory_ptr;
+  pthread_mutex_t pool_mutex;
 };
 
 
