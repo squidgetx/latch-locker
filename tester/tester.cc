5f3e13bcd776ed665e63cdf7bb0935abd5fceb8a
Minh Tri Pham <minhtri@pham.cz>
2016-04-21 18:44:36 -0400

    some tests for speed

--- tester/tester.cc
+++ tester/tester.cc
@@ -24,14 +24,68 @@ Tester::Tester() {
 void Tester::Run() {
   std::vector<std::pair<Key, LockRequest>> lock_requests;
 
-  int n = 10000; // number of lock requests
-  int k = 10; // number of distinct keys
-  double w = 0.5; // percentage of write locks
+  int n = 100000; // number of lock requests
+  int k; // number of distinct keys
+  double w; // percentage of write locks
 
+  k = 100;
+  w = 0.0;
+  std::cout << "high num of different keys, all shared locks " << std::endl;
   lock_requests = GenerateRequests(n, k, w);
   Benchmark(lock_requests);
   lock_requests.clear();
+  std::cout << "======" << std::endl;
 
+  k = 100;
+  w = 1.0;
+  std::cout << "high num of different keys, all exclusive locks " << std::endl;
+  lock_requests = GenerateRequests(n, k, w);
+  Benchmark(lock_requests);
+  lock_requests.clear();
+  std::cout << "======" << std::endl;
+
+  k = 100;
+  w = 0.5;
+  std::cout << "high num of different keys, mixed locks 50/50 " << std::endl;
+  lock_requests = GenerateRequests(n, k, w);
+  Benchmark(lock_requests);
+  lock_requests.clear();
+  std::cout << "======" << std::endl;
+
+  k = 5;
+  w = 0.0;
+  std::cout << "low num of different keys, all shared locks " << std::endl;
+  lock_requests = GenerateRequests(n, k, w);
+  Benchmark(lock_requests);
+  lock_requests.clear();
+  std::cout << "======" << std::endl;
+
+  k = 5;
+  w = 1.0;
+  std::cout << "low num of different keys, all exclusive locks " << std::endl;
+  lock_requests = GenerateRequests(n, k, w);
+  Benchmark(lock_requests);
+  lock_requests.clear();
+  std::cout << "======" << std::endl;
+
+  k = 5;
+  w = 0.5;
+  std::cout << "low num of different keys, mixed locks 50/50" << std::endl;
+  lock_requests = GenerateRequests(n, k, w);
+  Benchmark(lock_requests);
+  lock_requests.clear();
+  std::cout << "======" << std::endl;
+
+  // all different configs
+  // for (k = 5; k < 50; k+=5) {
+  //   for (w = 0.0; w <= 1.0; w += 0.2) {
+  //     std::cout << "100000 requests, "<< k << " keys, " << w*100 << "\% write locks" << std::endl;
+  //     lock_requests = GenerateRequests(n, k, w);
+  //     Benchmark(lock_requests);
+  //     lock_requests.clear();
+  //     std::cout << "======" << std::endl;
+  //   }
+  // }
 }
 
 std::vector<std::pair<Key, LockRequest>> Tester::GenerateRequests(int n, int k, double w) {
@@ -51,6 +105,7 @@ std::vector<std::pair<Key, LockRequest>> Tester::GenerateRequests(int n, int k, 
 void Tester::Benchmark(std::vector<std::pair<Key, LockRequest> > lock_requests) {
   LockManager *lm;
   // three types of mgr_s
+  std::string types[] = { "Global Lock", "Key Lock", "Latch-Free"  };
   for (int i = 0; i < 2; i++) {
     switch(i) {
       case 0:
@@ -65,7 +120,7 @@ void Tester::Benchmark(std::vector<std::pair<Key, LockRequest> > lock_requests) 
       default:
         break;
     }
-    double durations[2];
+    double throughput[2];
 
     // Record start time.
     double start = GetTime();
@@ -83,7 +138,7 @@ void Tester::Benchmark(std::vector<std::pair<Key, LockRequest> > lock_requests) 
 
     // Record end time
     double end = GetTime();
-    durations[0] = lock_requests.size() / (end-start);
+    throughput[0] = lock_requests.size() / (end-start);
 
 
     start = GetTime();
@@ -96,9 +151,10 @@ void Tester::Benchmark(std::vector<std::pair<Key, LockRequest> > lock_requests) 
 
     end = GetTime();
 
-    durations[1] = lock_requests.size() / (end-start);
+    throughput[1] = lock_requests.size() / (end-start);
 
-    std::cout << "Lock Manager " << i << ". Avg Acquire Duration: " << durations[0]
-                                      << ". Avg Release Duration: " << durations[1] << std::endl;
+    std::cout << "Lock Manager: " << types[i] << std::endl;
+    std::cout << "  Avg # Acquires / sec: " << int(throughput[0]) << std::endl
+              << "  Avg # Releases / sec: " << int(throughput[1]) << std::endl;
   }
 }
