98f9ab53ccd005e946a6e7bf99eb3702935dd946
squidgetx <sylvan.zheng@yale.edu>
2016-04-21 13:17:19 -0400

    memset global pool for initialization

--- hashtable/lock_pool.cc
+++ hashtable/lock_pool.cc
@@ -1,9 +1,11 @@
 #include "lock_pool.h"
+#include <string.h>
 
 LockPool::LockPool(int n) : capacity(n) {
   // For now, we just use a giant array
   memory_array = reinterpret_cast<TNode<LockRequest>*> (new char[
       sizeof(TNode<LockRequest>)*n]);
+  memset(memory_array, 0, sizeof(TNode<LockRequest>) * n);
   memory_ptr = memory_array;
   pthread_mutex_init(&pool_mutex, NULL);
 }
