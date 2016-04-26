e97d237738228873d68f3a172db2fc6908e2a360
David Hatch <dhatch387@gmail.com>
2016-04-26 16:48:49 -0400

    Retab txn.

--- txn/txn.cc
+++ txn/txn.cc
@@ -2,24 +2,24 @@
 #include "lock_managers/lock_manager.h"
 
 void Txn::Execute(LockManager *lm) {
-	// acquire phase
-	for (int i = 0; i < lr_vector.size(); i++) {
-		std::pair<Key, LockRequest> p = lr_vector[i];
+  // acquire phase
+  for (int i = 0; i < lr_vector.size(); i++) {
+    std::pair<Key, LockRequest> p = lr_vector[i];
 
-		Key k = p.first;
-		LockRequest r = p.second;
-		if (r.mode_ == SHARED) {
-		  lm->ReadLock(r.txn_, k);
-		} else if (r.mode_ == EXCLUSIVE) {
-		  lm->WriteLock(r.txn_, k);
-		}
-	}
+    Key k = p.first;
+    LockRequest r = p.second;
+    if (r.mode_ == SHARED) {
+      lm->ReadLock(r.txn_, k);
+    } else if (r.mode_ == EXCLUSIVE) {
+      lm->WriteLock(r.txn_, k);
+    }
+  }
 
-	// release phase
-	for (int i = 0; i < lr_vector.size(); i++) {
-	  std::pair<Key, LockRequest> p = lr_vector[i];
-	  Key k = p.first;
-	  LockRequest r = p.second;
-	  lm->Release(r.txn_, k);
-	}
+  // release phase
+  for (int i = 0; i < lr_vector.size(); i++) {
+    std::pair<Key, LockRequest> p = lr_vector[i];
+    Key k = p.first;
+    LockRequest r = p.second;
+    lm->Release(r.txn_, k);
+  }
 }
