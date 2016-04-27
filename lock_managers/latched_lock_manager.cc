8ad7c4849fcef98b57888d23cd573fe9e3475638
David Hatch <dhatch387@gmail.com>
2016-04-26 22:06:49 -0400

    Make latched manager use same release alg.

--- lock_managers/latched_lock_manager.cc
+++ lock_managers/latched_lock_manager.cc
@@ -67,67 +67,41 @@ TNode<LockRequest>* LatchedLockManager::ReadLock(Txn* txn, const Key key) {
 }
 
 void LatchedLockManager::Release(Txn* txn, const Key key) {
-  Pthread_mutex_guard guard(KeyMutex(key));
 
   LockRequestLinkedList * list = lock_table.get_list(key);
-  if (list == NULL || list->empty()) {
-    // don't bother releasing non existent lock
-    return;
-  }
-
-
-  // First find the txn in the lock request list
-  TNode<LockRequest> * current;
-  // True if txn is the first holder of the lock.
-  bool firstLockHolder = true;
-  for(current = list->head; current != NULL; current = current->next) {
-    if (current->data.txn_ == txn) {
-      break;
-    }
-    firstLockHolder = false;
-  }
-
-  if (current == NULL) {
-    // abort if the txn doesn't have a lock on this item
-    return;
-  }
-
-  int mode = current->data.mode_;
-  //assert(current->data.state_ == ACTIVE);
-  TNode<LockRequest> * next = current->next;
-  list->deleteRequest(current);
-
-  if (next == NULL) {
-    // nothing left to do if no other txns waiting on this lock
-    return;
-  }
-
-  if (mode == SHARED) {
-    // don't do anything if you aren't the last shared lock holder
-    if (next->data.mode_ == SHARED) {
-      return;
-    }
-  }
-
-  // At this point, the released lock was either
-  // a) an EXCLUSIVE lock or
-  // b) a SHARED lock, AND the next txn is an EXCLUSIVE lock
-
-  if (next->data.mode_ == SHARED) {
-    // Grant the lock to all the locks in this segment waiting for
-    // a SHARED lock
-    for (; next != NULL; next = next->next) {
-      if (next->data.mode_ != SHARED) {
+  TNode<LockRequest>* req = list->head;
+  TNode<LockRequest>* toRelease;
+  bool foundExcl = false;
+  bool foundOurTxn = false;
+  while (req != NULL) {
+    if (!foundOurTxn && req->data.txn_ == txn) {
+      // If we are a SHARED lock
+      //  then any locks after us are already in the correct state
+      //  and we can quit
+      //If we are an EXCLUSIVE lock AND we found an exclusive lock
+      //  then any locks after us are already in the correct state
+      // If we are an EXCLUSIVE lock AND we found NO exclusive locks
+      //  then we need to grant any shared locks after us UNTIL we find
+      //  an exclusive lock (or we are at the end of the list)
+      foundOurTxn = true;
+      toRelease = req;
+      {
+        Pthread_mutex_guard guard(KeyMutex(key));
+        list->deleteRequest(req);
+      }
+      if (foundExcl == true || req->data.mode_ == SHARED) {
         break;
       }
-    next->data.state_ = ACTIVE;
+    } else if (req->data.mode_ == EXCLUSIVE) {
+      foundExcl = true;
+      if (foundOurTxn) {
+        break;
+      }
+    } else if (foundOurTxn) {
+      req->data.state_ = ACTIVE;
     }
-
-  } else if (firstLockHolder) {
-    // Just grant to the lock to this request (exclusive lock)
-    next->data.state_ = ACTIVE;
+    req = req->next;
   }
-
 }
 
 LockState LatchedLockManager::CheckState(const Txn* txn, const Key key) {
