cdd57bb5a864fc29ee22edab66bc582e0fa2f7ab
squidgetx <sylvan.zheng@yale.edu>
2016-05-03 16:50:10 -0400

    run all tests

--- tester/correct_test.cc
+++ tester/correct_test.cc
@@ -167,7 +167,7 @@
    // Test the global lcok manager
    // three types of mgr_s
    LockManager * lm;
-   for (int i = 0; i < 2; i++) {
+   for (int i = 0; i < 3; i++) {
      switch(i) {
        case 0:
          lm = new GlobalLockManager(100);
